2219df825d05662b39209e67909b670340c5e60c
joseotol <josemanuel.otero.oliveira.alu@iesfernandoaguilar.es>
2020-10-29 21:36:42 +0100

    Mejora script con posiciones pares

--- lista_si_no.py
+++ lista_si_no.py
@@ -1,8 +1,15 @@
-li = [5, 8, 9, 10, 7, 8, 5, 7, 22, 61, 2, 4]
+li = [5, 8, 9, 10, 7]
 lopinto=True
-
 
 for elemento in li:
 	if lopinto:
 		print (elemento)
 	lopinto=not lopinto
+
+
+indice=0
+
+for elemento in li:
+	if indice%2==0:
+		print(elemento)
+	indice=indice+1
